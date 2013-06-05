0ac3a23622acaf38c8d08252317f97303fd22f91
Ilja Sidoroff <ilja.sidoroff@helsinki.fi>
2013-06-05 16:08:38 +0300

    Minor things

--- matasano1.go
+++ matasano1.go
@@ -5,18 +5,28 @@ package matasano
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 )
 
-func HexToBase64(hex string) string {
-	return hex
-//	return base64.EncodeToString(hex)
+// Convert hex string to base64 encoding
+func HexToBase64(s string) string {
+	bytes, err := hex.DecodeString(s)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return ""
+	}
+	str := base64.StdEncoding.EncodeToString(bytes)
+	return str
 }
 
 
+// Convert base64 encoded string to hex representation
 func Base64ToHex(s string) string {
 	return s
 }
+
+
 
 func base64ToBytes(s string) []byte {
 	bytes, err := base64.StdEncoding.DecodeString(s)
--- matasano_test.go
+++ matasano_test.go
@@ -3,7 +3,7 @@
 package matasano
 
 import (
-//	"bytes"
+	//	"bytes"
 	"testing"
 )
 
@@ -12,12 +12,9 @@ var encodingTests = []struct {
 	b64 string
 }{
 	{"", ""},
-	{"a", "a"},
+	{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+		"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
 }
-
-var testCases = [2]string{
-	"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
-	"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"}
 
 func TestHexToBase64(t *testing.T) {
 	for _, tt := range encodingTests {
