5cd4ec24e83daf1631ba8c04e0c2bcefe432ac13
Ilja Sidoroff <ilja.sidoroff@helsinki.fi>
2013-06-06 17:14:51 +0300

    First two tasks. Some work on task 3

--- matasano1.go
+++ matasano1.go
@@ -7,6 +7,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"unicode/utf8"
 )
 
 // Convert hex string to base64 encoding
@@ -20,24 +21,108 @@ func HexToBase64(s string) string {
 	return str
 }
 
-
 // Convert base64 encoded string to hex representation
 func Base64ToHex(s string) string {
-	return s
-}
-
-
-
-func base64ToBytes(s string) []byte {
 	bytes, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
 		fmt.Println("error: ", err)
-		return nil
+		return ""
 	}
-	return bytes
+	str := hex.EncodeToString(bytes)
+	return str
 }
 
-func hexToBytes(hex string) []byte {
-	b := make([]byte, len(hex) / 2)
-	return b
+// XOR two byte slices
+func FixedXOR(a, b []byte) []byte {
+	if len(a) != len(b) {
+		fmt.Println("Slices aren't the same length (%v != %v)", len(a), len(b))
+		return nil
+	}
+	res := make([]byte, len(a))
+	for i := range a {
+		res[i] = a[i] ^ b[i]
+	}
+	return res
+}
+
+// expand one byte to l sized byte-slice
+func expandByte(b byte, l int) []byte {
+	
+	res := make([]byte, l)
+	for i := range res {
+		res[i] = b
+	}
+	return res
+}
+
+// convert rune to a byte, discard extra bytes
+func convertRune(c rune) byte {
+	l := utf8.RuneLen(c)
+	if l != 1 {
+		fmt.Println("Warning: discarding extra %d bytes in character", l - 1)
+	}
+	buf := make([]byte, l)
+	buf = c
+	return buf[0]
+}
+
+// Decrypt ciphertext encrypted with single characeter XOR
+func SingleCharacterXORCipher(cipher, keyspace string) {
+
+	cipherBytes, err := hex.DecodeString(cipher)
+
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+	cipherLen := len(cipherBytes)
+
+	// loop over letters and compute character histogram for decrypted text
+	for _, c := range keyspace {
+		// multiply the key
+		b := convertRune(c) 
+		key := expandByte(b, cipherLen)
+		res := FixedXOR(cipherBytes, key)
+		
+	}
+
+	// sort histograms accoring to distance to the target histogram
+}
+
+// 
+func Solve3() {
+	cipher := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	keyspace := "abcdefghijklmnopqrtstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYX"
+
+	englishLetterFrequency := map[string]float64 {
+		"e" : 12.02,
+		"t" : 9.10,
+		"a" : 8.12,
+		"o" : 7.68,
+		"i" : 7.31,
+		"n" : 6.95,
+		"s" : 6.28,
+		"r" : 6.02,
+		"h" : 5.92,
+		"d" : 4.32,
+		"l" : 3.98,
+		"u" : 2.88,
+		"c" : 2.71,
+		"m" : 2.61,
+		"f" : 2.30,
+		"y" : 2.11,
+		"w" : 2.09,
+		"g" : 2.03,
+		"p" : 1.82,
+		"b" : 1.49,
+		"v" : 1.11,
+		"k" : 0.69,
+		"x" : 0.17,
+		"q" : 0.11,
+		"j" : 0.10,
+		"z" : 0.07, 
+	}
+
+
+	SingleCharacterXORCipher(cipher, keyspace)
 }
--- matasano_test.go
+++ matasano_test.go
@@ -3,9 +3,14 @@
 package matasano
 
 import (
-	//	"bytes"
+	"bytes"
+	"encoding/hex"
 	"testing"
 )
+
+//
+// tests for task 1
+//
 
 var encodingTests = []struct {
 	hex string
@@ -34,16 +39,37 @@ func TestBase64ToHex(t *testing.T) {
 	}
 }
 
-/*
+//
+// tests for task 2
+//
 
-func TestBase64ToHex(t *testing.T) {
+var fixedXORTests = []struct {
+	a   string
+	b   string
+	res string
+}{
+	{"1c0111001f010100061a024b53535009181c",
+		"686974207468652062756c6c277320657965",
+		"746865206b696420646f6e277420706c6179"},
+	{"ffff",
+		"ffff",
+		"0000"},
+}
 
-	const in = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	const out = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-	var out_b = []byte(out)
-
-	if x := base64ToHex(in); !bytes.Equal(x, out_b) {
-		t.Errorf("Base64ToHex(%v) = %v, want %v", in, x, out_b)
+func TestFixedXOR(t *testing.T) {
+	for _, tt := range fixedXORTests {
+		a, _ := hex.DecodeString(tt.a)
+		b, _ := hex.DecodeString(tt.b)
+		r, _ := hex.DecodeString(tt.res)
+		res := FixedXOR(a, b)
+		if !bytes.Equal(res, r) {
+			t.Errorf("FixedXOR(%v, %v) = %v, want %v", tt.a, tt.b, hex.EncodeToString(res), tt.res)
+		}
 	}
 }
-*/
+
+//
+// tests for task 3
+//
+
+var cipher = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
