f319608588a502cc24cc5f98ff376a1bdbf84ce7
Ilja Sidoroff <ilja.sidoroff@helsinki.fi>
2013-08-15 16:46:09 +0300

    Fixed case-issues

--- src/test/scala/matasano/XORSuite.scala
+++ src/test/scala/matasano/XORSuite.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class XORSuite extends FunSuite {
 
-  val XORs = 
+  val xors = 
     Array(
       ("1c0111001f010100061a024b53535009181c",
         "686974207468652062756c6c277320657965",
@@ -12,48 +12,48 @@ class XORSuite extends FunSuite {
     )
 
   test("FixedXOR hex strings, result in bytes") {
-    for ((x1, x2, res) <- XORs)
-      assert(XOR.FixedXOR(Tools.decodeHex(x1), Tools.decodeHex(x2)) === Tools.decodeHex(res))
+    for ((x1, x2, res) <- xors)
+      assert(XOR.fixedXOR(Tools.decodeHex(x1), Tools.decodeHex(x2)) === Tools.decodeHex(res))
   }
 
   test("FixedXOR hex string, result in string") {
-    for ((x1, x2, res) <- XORs)
-      assert(XOR.FixedXOR(x1, x2) === res)
+    for ((x1, x2, res) <- xors)
+      assert(XOR.fixedXOR(x1, x2) === res)
   }
 
-  val SingleCharacterXORs =
+  val singleCharacterXORs =
     Array(
       ("ff00ff", "00", "ff00ff"),
       ("ff00ff", "ff", "00ff00")
     )
 
   test("SingleCharacterXOR, result in bytes") {
-    for ((x, c, res) <- SingleCharacterXORs)
-      assert(XOR.SingleCharacterXOR(Tools.decodeHex(x), Tools.decodeHex(c).head) === Tools.decodeHex(res))
+    for ((x, c, res) <- singleCharacterXORs)
+      assert(XOR.singleCharacterXOR(Tools.decodeHex(x), Tools.decodeHex(c).head) === Tools.decodeHex(res))
   }
 
-  val SingleCharacterCharXORs =
+  val singleCharacterCharXORs =
     Array(
       ("ff00ff", 'a', "9e619e")
     )
 
   test("SingleCharacterXOR, result in string") {
-    for ((x, c, res) <- SingleCharacterCharXORs)
-      assert(XOR.SingleCharacterXOR(x, c) === res)
+    for ((x, c, res) <- singleCharacterCharXORs)
+      assert(XOR.singleCharacterXOR(x, c) === res)
   }
   test("RepeatKey (bytes)") {
-    assert(XOR.RepeatKey(Array[Byte](1,2,3,4,5), Array[Byte](1)) === Array[Byte](1,1,1,1,1))
-    assert(XOR.RepeatKey(Array[Byte](1,2,3,4,5), Array[Byte](1,2)) === Array[Byte](1,2,1,2,1))
+    assert(XOR.repeatKey(Array[Byte](1,2,3,4,5), Array[Byte](1)) === Array[Byte](1,1,1,1,1))
+    assert(XOR.repeatKey(Array[Byte](1,2,3,4,5), Array[Byte](1,2)) === Array[Byte](1,2,1,2,1))
   }
 
   test("RepeatKey (strings)") {
-    assert(XOR.RepeatKey("abcde", "a") === "aaaaa")
-    assert(XOR.RepeatKey("abcde", "ab") === "ababa")
+    assert(XOR.repeatKey("abcde", "a") === "aaaaa")
+    assert(XOR.repeatKey("abcde", "ab") === "ababa")
   }
 
   test("RepeatingKeyXOR") {
     assert(Tools.encodeHex(
-      XOR.RepeatingKeyXOR("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE")) === 
+      XOR.repeatingKeyXOR("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE")) === 
       "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
   }
 }
