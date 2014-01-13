57fe502247438b94359c58bed803fcd0dbf28e06
Ilja Sidoroff <ilja.sidoroff@helsinki.fi>
2014-01-13 16:11:56 +0200

    Challenge 1 working

--- haskell/src/Matasano.hs
+++ haskell/src/Matasano.hs
@@ -1,15 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Matasano where
 --
 -- Matasano crypto challenges
 --
 
---import Data.ByteString.Char8 ()
---import Data.ByteString.Base64 (encode, decode)
+import Data.ByteString
+import Data.ByteString.Char8 ()
+import Data.ByteString.Base16 (encode, decode)
+import Data.ByteString.Base64
 
 -- Challenge 1 
-hexToBase64 :: String -> String
-hexToBase64 hStr = hStr
+hexToBase64 :: ByteString -> ByteString
+hexToBase64 hStr = Data.ByteString.Base64.encode $ fst (Data.ByteString.Base16.decode hStr)
 
-base64ToHex :: String -> String
-base64ToHex b64Str = b64Str
+base64ToHex :: ByteString -> ByteString
+base64ToHex b64Str = case Data.ByteString.Base64.decode b64Str of
+                         Left msg -> error "str"           
+                         Right str -> Data.ByteString.Base16.encode str
+
 
